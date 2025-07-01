68aaa5f5c19b20aeac23313a53dba2df855d661a
mmije0ng <victon777@naver.com>
2025-07-01 20:39:39 +0900

    [#1] Init: 프로젝트 세팅

---
+++ src/test/java/com/server/money_touch/MoneyTouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.money_touch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
