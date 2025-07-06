f565a918b42a7e516f0d6bd7e57f7a991495d507
Yujin1219 <jin-1219@naver.com>
2025-07-06 15:14:51 +0900

    [#15] Feat: 공감 엔터티 연관관계 매핑

--- src/main/java/com/server/money_touch/domain/consumptionRecord/entity/Reaction.java
+++ src/main/java/com/server/money_touch/domain/consumptionRecord/entity/Reaction.java
@@ -1,6 +1,7 @@
 package com.server.money_touch.domain.consumptionRecord.entity;
 
 import com.server.money_touch.domain.consumptionRecord.enums.ReactionType;
+import com.server.money_touch.domain.user.entity.User;
 import com.server.money_touch.global.apiPayload.code.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -11,11 +12,18 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "consumption_record_id"})
+}) // 한 유저는 한 피드당 하나의 리액션만 가능
 public class Reaction extends BaseEntity {
 
-    // 유저 관계 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-    // 소비기록 관계 매핑
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "consumption_record_id", nullable = false)
+    private ConsumptionRecord consumptionRecord;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
