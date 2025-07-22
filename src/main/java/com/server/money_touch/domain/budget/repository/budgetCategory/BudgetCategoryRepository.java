d897a7ecf05c3d88c454d94ed129d801424ddde8
mmije0ng <victon777@naver.com>
2025-07-23 00:00:08 +0900

    [#64] Fix: 예산 수정 시 삭제된 카테고리가 있다면 반영되도록 수정

--- src/main/java/com/server/money_touch/domain/budget/repository/budgetCategory/BudgetCategoryRepository.java
+++ src/main/java/com/server/money_touch/domain/budget/repository/budgetCategory/BudgetCategoryRepository.java
@@ -3,6 +3,7 @@ package com.server.money_touch.domain.budget.repository.budgetCategory;
 import com.server.money_touch.domain.budget.entity.Budget;
 import com.server.money_touch.domain.budget.entity.BudgetCategory;
 import com.server.money_touch.domain.budget.enums.CategoryType;
+import com.server.money_touch.domain.consumptionRecord.entity.ConsumptionCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -26,5 +27,7 @@ public interface BudgetCategoryRepository extends JpaRepository<BudgetCategory, 
     """)
     List<BudgetCategory> findAllWithCategoryByBudgetAndType(@Param("budget") Budget budget,
                                                             @Param("type") CategoryType type);
+
+    long countByConsumptionCategory(ConsumptionCategory category);
 }
 
