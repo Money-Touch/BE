a9caba2c80cda2f55b426d79b0a2195534dcb8a6
Yujin1219 <jin-1219@naver.com>
2025-07-31 00:43:50 +0900

    [#86] Feat: 마이페이지 유저정보조회 기능 구현

--- src/main/java/com/server/money_touch/domain/user/converter/UserConverter.java
+++ src/main/java/com/server/money_touch/domain/user/converter/UserConverter.java
@@ -1,5 +1,7 @@
 package com.server.money_touch.domain.user.converter;
 
+import com.server.money_touch.domain.badge.entity.Badge;
+import com.server.money_touch.domain.user.dto.UserResponse;
 import com.server.money_touch.domain.user.entity.LocalLogin;
 import com.server.money_touch.domain.user.entity.SocialLogin;
 import com.server.money_touch.domain.user.entity.User;
@@ -90,5 +92,14 @@ public class UserConverter {
         return socialLogin;
     }
 
+    // 마이페이지용
+    public static UserResponse.MyPageResponseDTO toMyPageResponseDTO(User user, Badge badge){
+
+        return UserResponse.MyPageResponseDTO.builder()
+                .nickname(user.getNickname())
+                .profileImgUrl(user.getProfileImgUrl())
+                .representativeBadgeImageUrl(badge != null ? badge.getImageUrl() : null)
+                .build();
+    }
 
 }
